c9f35867d50de12339680b02ad34f377fd41d9f1
Salvatore Rago <salvatorerago373@gmail.com>
2024-06-26 12:25:41 +0200

    working lwr scheme

--- simulink robot/test.m
+++ simulink robot/test.m
@@ -23,8 +23,8 @@ end
 r = KukaLbr4pRobot();
 setGlobalRobot(r);
 
-robot_model = importrobot('iiwa14.urdf'); % Load KUKA iiwa14
-% robot_model = importrobot("../robot_model/kuka-lwr-4plus/model1.urdf"); % Load KUKA LWR 4+
+% robot_model = importrobot('iiwa14.urdf'); % Load KUKA iiwa14
+robot_model = importrobot("../robot_model/kuka-lwr-4plus/model1.urdf"); % Load KUKA LWR 4+
 robot_model.DataFormat = 'column';
 robot_model.Gravity = [0, -9.80665, 0];
 
@@ -61,7 +61,8 @@ for i=2:8
 end
 
 % smimport(robot_model)
-open_system('robot_simulink.slx');
+% open_system('robot_simulink.slx'); % iiwa14
+open_system('./kuka_test/lwr_scheme.slx'); % lwr4
 
 % Load the Simulink file:
 % sfile = "./simulink_proj/robot_model.slx";

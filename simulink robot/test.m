bbe9e1d3f3a5766e7c8d8b0c752361997b704a2c
Salvatore Rago <salvatorerago373@gmail.com>
2024-06-27 17:24:37 +0200

    LWR: add trajectory acceleration component in a

--- simulink robot/test.m
+++ simulink robot/test.m
@@ -19,6 +19,18 @@ global robot;
 r = robot;
 end
 
+% Trajectory
+% syms t R T C1 C2 C3 real
+% radius = R; % 0.5;
+% circle_center = [C1; C2; C3]; % [0; 0; 0.4];
+% u_circle_plane = [1; 0; 0];  % must be unit vec and orth to v
+% v_circle_plane = [0; 1; 0];  % must be unit vec and orth to u
+% % T = 10;  % trajectory duration in seconds
+% path(t) = circle_center + u_circle_plane*radius*cos((t/T) * (2*pi)) + v_circle_plane*radius*sin((t/T) * (2*pi));
+% path_dot(t) = diff(path, t);
+% path_ddot(t) = diff(path_dot, t);
+
+
 % Intialize the KUKA robot
 r = KukaLbr4pRobot();
 setGlobalRobot(r);
@@ -47,24 +59,24 @@ setFixedTransform(robot_model.Bodies{7}.Joint,dhparams(6,:),'dh');
 setFixedTransform(robot_model.Bodies{8}.Joint,dhparams(7,:),'dh');
 
 % Set robot dynamic parameters (equal to KUKA LWR4+)
-vals = load('../resources/paper_vals.mat').d;
-for i=2:8
-    j = i-1;
-    mass_key = strcat("m", int2str(j));
-    c_i_x = strcat("c", int2str(j), ',1');
-    c_i_y = strcat("c", int2str(j), ',2');
-    c_i_z = strcat("c", int2str(j), ',3');
-    I_i_xx = strcat("I", int2str(j), ',1_1');
-    I_i_xy = strcat("I", int2str(j), ',1_2');
-    I_i_xz = strcat("I", int2str(j), ',1_3');
-    I_i_yy = strcat("I", int2str(j), ',2_2');
-    I_i_yz = strcat("I", int2str(j), ',2_3');
-    I_i_zz = strcat("I", int2str(j), ',3_3');
-
-    robot_model.Bodies{i}.Mass = vals(mass_key);
-    robot_model.Bodies{i}.CenterOfMass = [vals(c_i_x); vals(c_i_y); vals(c_i_z)];
-    robot_model.Bodies{i}.Inertia = [vals(I_i_xx), vals(I_i_yy), vals(I_i_zz), vals(I_i_yz), vals(I_i_xz), vals(I_i_xy)];
-end
+% vals = load('../resources/paper_vals.mat').d;
+% for i=2:8
+%     j = i-1;
+%     mass_key = strcat("m", int2str(j));
+%     c_i_x = strcat("c", int2str(j), ',1');
+%     c_i_y = strcat("c", int2str(j), ',2');
+%     c_i_z = strcat("c", int2str(j), ',3');
+%     I_i_xx = strcat("I", int2str(j), ',1_1');
+%     I_i_xy = strcat("I", int2str(j), ',1_2');
+%     I_i_xz = strcat("I", int2str(j), ',1_3');
+%     I_i_yy = strcat("I", int2str(j), ',2_2');
+%     I_i_yz = strcat("I", int2str(j), ',2_3');
+%     I_i_zz = strcat("I", int2str(j), ',3_3');
+% 
+%     robot_model.Bodies{i}.Mass = vals(mass_key);
+%     robot_model.Bodies{i}.CenterOfMass = [vals(c_i_x); vals(c_i_y); vals(c_i_z)];
+%     robot_model.Bodies{i}.Inertia = [vals(I_i_xx), vals(I_i_yy), vals(I_i_zz), vals(I_i_yz), vals(I_i_xz), vals(I_i_xy)];
+% end
 
 % smimport(robot_model)
 if robot_to_try == "iiwa14"

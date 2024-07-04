d0b21d7573c020e99ba528e01e8abef045f7aac1
Salvatore Rago <salvatorerago373@gmail.com>
2024-07-04 18:07:32 +0200

    Added damping and 3d simul to acc controller

--- simulink_robot2acc/example.m
+++ simulink_robot2acc/example.m
@@ -20,7 +20,7 @@ radius = 0.5;
 circle_center = [0; 0; 0.4];
 u_circle_plane = [1; 0; 0];  % must be unit vec and orth to v
 v_circle_plane = [0; 1; 0];  % must be unit vec and orth to u
-T = 10;  % trajectory duration in seconds
+T = simulation_time;  % trajectory duration in seconds
 path = @(t) circle_center + u_circle_plane*radius*cos((t/T) * (2*pi)) + v_circle_plane*radius*sin((t/T) * (2*pi));
 path_dot = @(t) -u_circle_plane*radius*(2*pi/T)*sin((t/T) * (2*pi)) + v_circle_plane*radius*(2*pi/T)*cos((t/T) * (2*pi));
 path_ddot = @(t) -u_circle_plane*radius*(2*pi/T)*(2*pi/T)*cos((t/T) * (2*pi)) - v_circle_plane*radius*(2*pi/T)*(2*pi/T)*sin((t/T) * (2*pi));
@@ -60,3 +60,13 @@ ylim([-2, 2]);
 % Save the figures
 exportgraphics(f1, "./fig/q.pdf");
 exportgraphics(f2, "./fig/u.pdf");
+
+% Display the trajectory in Simscape
+n = 10;
+omega = linspace(0, T, n);
+data_points = "[";
+for i=1:n-1
+    data_points = data_points + num2str(double(path(omega(i))')) + "; ";
+end
+data_points = data_points + "]";
+set_param('lwr_scheme/RobotModel/trajectorySpline', 'DataPoints', data_points);

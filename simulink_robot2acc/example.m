676ce5c96e8694e57a043a0955cd113b07505214
Salvatore Rago <salvatorerago373@gmail.com>
2024-07-05 12:34:03 +0200

    Acceleration controller: added saturation and plots

--- simulink_robot2acc/example.m
+++ simulink_robot2acc/example.m
@@ -1,3 +1,5 @@
+clear;
+clc;
 % Example script on how to use the MATLAB Robotics Toolbox for a trajectory tracking control task in joint space
 
 %% Load the robot
@@ -13,7 +15,8 @@ acc_low_bound = -20;
 
 %% Simulation parameters
 simulation_time = 10;
-q_0             = randn(n, 1); % Initial configuration
+q_0             = zeros(n, 1); % Initial configuration
+q_0(4,1) = pi/2;
 dq_0            = zeros(n, 1); % Initial velocity
 
 %% Define a joint trajectory as a cubic spline from q_start to q_end with zero velocities
@@ -22,48 +25,14 @@ dq_0            = zeros(n, 1); % Initial velocity
 % syms t real
 radius = 0.5;
 circle_center = [0; 0; 0.4];
-u_circle_plane = [1; 0; 0];  % must be unit vec and orth to v
-v_circle_plane = [0; 1; 0];  % must be unit vec and orth to u
+% u_circle_plane = [1; 0; 0];  % must be unit vec and orth to v
+% v_circle_plane = [0; 1; 0];  % must be unit vec and orth to u
+u_circle_plane = [sqrt(2)/2; 0; sqrt(2)/2];  % must be unit vec and orth to v
+v_circle_plane = [0; sqrt(2)/2; sqrt(2)/2];  % must be unit vec and orth to u
 T = simulation_time;  % trajectory duration in seconds
 path = @(t) circle_center + u_circle_plane*radius*cos((t/T) * (2*pi)) + v_circle_plane*radius*sin((t/T) * (2*pi));
 path_dot = @(t) -u_circle_plane*radius*(2*pi/T)*sin((t/T) * (2*pi)) + v_circle_plane*radius*(2*pi/T)*cos((t/T) * (2*pi));
 path_ddot = @(t) -u_circle_plane*radius*(2*pi/T)*(2*pi/T)*cos((t/T) * (2*pi)) - v_circle_plane*radius*(2*pi/T)*(2*pi/T)*sin((t/T) * (2*pi));
-
-%% Open the simulink model, simulate it and store the results for plotting purposes
-open('model1acc.slx');
-out = sim('model1acc.slx');
-save("simulation_results", "out");
-
-%% Plotting the results
-load("simulation_results.mat");
-
-% Configuration
-f1 = figure; grid on; box on; hold on;
-plot(out.q.Time, out.q.Data, "LineWidth", 2);
-pbaspect([2, 1, 1]);
-xlabel("Time [s]", "Interpreter", "latex", "FontSize", 14);
-ylabel("Configuration [rad]", "Interpreter", "latex", "FontSize", 14);
-pl = plot(out.q_d.Time, out.q_d.Data, "k--", "LineWidth", 2);
-set(pl, {'HandleVisibility'},  [{"on"}; repmat({"off"}, n-1, 1)]); % Keep the visibility in the legend of only one reference
-legend([arrayfun(@(i) sprintf("$q_{%d}$", i), 1:n, "UniformOutput", false)'; {"$q_{d}$"}], ...
-        "Interpreter", "latex", ...
-       "FontSize", 14, ...
-       "NumColumns", 6, ...
-       "Location", "northoutside");
-
-% Control action
-f2 = figure; grid on; box on; hold on;
-plot(out.tau.Time, out.tau.Data, "LineWidth", 2);
-pbaspect([2, 1, 1]);
-xlabel("Time [s]", "Interpreter", "latex", "FontSize", 14);
-ylabel("Torque [Nm]", "Interpreter", "latex", "FontSize", 14);
-grid on; box on;
-ylim([-2, 2]);
-
-
-% Save the figures
-exportgraphics(f1, "./fig/q.pdf");
-exportgraphics(f2, "./fig/u.pdf");
 
 % Display the trajectory in Simscape
 n = 10;
@@ -73,4 +42,61 @@ for i=1:n-1
     data_points = data_points + num2str(double(path(omega(i))')) + "; ";
 end
 data_points = data_points + "]";
-set_param('lwr_scheme/RobotModel/trajectorySpline', 'DataPoints', data_points);
+set_param('model1acc/RobotModel/trajectorySpline', 'DataPoints', data_points);
+
+%% Open the simulink model, simulate it and store the results for plotting purposes
+open('model1acc.slx');
+out = sim('model1acc.slx');
+save("simulation_results", "out");
+
+%% Plotting the results
+load("simulation_results.mat");
+
+f1 = figure; grid on; box on; hold on;
+title("Configuration plot", "FontName", "courier", "FontSize", 14);
+pl = plot(out.q.Time, out.q.Data, "LineWidth", 2);
+pbaspect([2, 1, 1]);
+xlabel("Time [s]", "Interpreter", "latex", "FontSize", 14);
+ylabel("Configuration [rad]", "Interpreter", "latex", "FontSize", 14);
+% pl = plot(out.q_d.Time, out.q_d.Data, "k--", "LineWidth", 2);
+% set(pl, {'HandleVisibility'},  [{"on"}; repmat({"off"}, n-1, 1)]); % Keep the visibility in the legend of only one reference
+legend([arrayfun(@(i) sprintf('$q_{%d}$', i), 1:n, "UniformOutput", false)'; {'$q_{d}$'}], ...
+        "Interpreter", "latex", ...
+       "FontSize", 14, ...
+       "NumColumns", 6, ...
+       "Location", "southoutside");
+
+% Control action
+f2 = figure; grid on; box on; hold on;
+title("Joint Acceleration plot", "FontName", "courier", "FontSize", 14);
+plot(out.tau.Time, reshape(out.tau.Data, 7, size(out.tau.Time, 1))', "LineWidth", 2);
+pbaspect([2, 1, 1]);
+xlabel("Time [s]", "Interpreter", "latex", "FontSize", 14);
+ylabel("Joint Acceleration [$\frac{rad}{s^2}$]", "Interpreter", "latex", "FontSize", 14);
+legend([arrayfun(@(i) sprintf('$\\ddot{q_{%d}}$', i), 1:n, "UniformOutput", false)'; {'$\\ddot{q_{d}}$'}], ...
+        "Interpreter", "latex", ...
+       "FontSize", 14, ...
+       "NumColumns", 6, ...
+       "Location", "southoutside");
+grid on; box on;
+ylim([acc_low_bound-10, acc_up_bound+10]);
+
+% Plot cartesian error
+f3 = figure; grid on; box on; hold on;
+title("Cartesian Error plot", "FontName", "courier", "FontSize", 14);
+plot(out.cart_err.Time, reshape(out.cart_err.Data, 3, size(out.tau.Time, 1))', "LineWidth", 2);
+pbaspect([2, 1, 1]);
+xlabel("Time [s]", "Interpreter", "latex", "FontSize", 14);
+ylabel("Cartesian Error [m]", "Interpreter", "latex", "FontSize", 14);
+legend([arrayfun(@(i) sprintf('$e_{%d}$', i), 1:n, "UniformOutput", false)'; {'$e_{d}$'}], ...
+        "Interpreter", "latex", ...
+       "FontSize", 14, ...
+       "NumColumns", 6, ...
+       "Location", "southoutside");
+grid on; box on;
+
+
+% Save the figures
+exportgraphics(f1, "./fig/q.pdf");
+exportgraphics(f2, "./fig/acc.pdf");
+exportgraphics(f3, "./fig/cart_err.pdf");
